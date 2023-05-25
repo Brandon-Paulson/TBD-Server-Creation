414356e1461ec534f6161a117b81baef219bea0f
Brandon Paulson <brandonpaulson28@gmail.com>
2023-05-24 20:21:01 -0500

    added in a database and get request for messages

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express');
-
+const pq = require('pg')
 const app = express();
 
 const PORT= process.env.PORT || 28714;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const pool = new pg.Pool({
+    // The name of your database. This will change for every app!
+    database: 'TBDServer', 
+    ssl: true,
+    host: 'db.bit.io',
+    // Postgres listens for network connections on port 5432, by default
+    port: 5432,
+    username: CrashAbandoncoot,
+    password: DB_PASSWORD
+});
+
 
 app.get('/ping', (req, res) =>{
     console.log('Request for status made');
     res.sendStatus(204);
+});
+
+app.get('/messages', (req, res) => {
+    let queryText = 'SELECT * FROM messages;';
+    pool.query(queryText)
+    .then((result)=> {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('Hey there was an error', error);
+        res.sendStatus(500);
+    })
 });
 
 
