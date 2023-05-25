a4ac4cf7ddcb74d3f8e64645168af5685a201b8c
Brandon Paulson <brandonpaulson28@gmail.com>
2023-05-24 19:22:19 -0500

    staged for creating heroku

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require('express');
 
+const app = express();
+
+const PORT= process.env.PORT || 8000
+
+
+app.listen(PORT, () => {
+    console.log('The App is listening on:', PORT)
+});
